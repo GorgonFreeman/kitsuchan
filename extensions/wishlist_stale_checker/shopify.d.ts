e6411c556d4dca03e562ab3258c686fd17c4702b
Benjamin Cofrancesco <benjamin@whitefoxboutique.com>
2026-06-05 10:23:45 +1000

    chore: types lol

--- extensions/wishlist_stale_checker/shopify.d.ts
+++ extensions/wishlist_stale_checker/shopify.d.ts
@@ -5,3 +5,9 @@ declare module './src/WishlistStalenessCheck.jsx' {
   const shopify: import('@shopify/ui-extensions/admin.customer-details.action.render').Api;
   const globalThis: { shopify: typeof shopify };
 }
+
+//@ts-ignore
+declare module './src/WishlistBulkStalenessCheck.jsx' {
+  const shopify: import('@shopify/ui-extensions/admin.customer-index.selection-action.render').Api;
+  const globalThis: { shopify: typeof shopify };
+}
