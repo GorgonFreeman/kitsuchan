8bf6b44c9cf2433d5ac648b80e92882f2ad0ceeb
John <29110105+GorgonFreeman@users.noreply.github.com>
2026-06-22 17:37:56 +1000

    compose inventory list action placeholder

--- extensions/pvx_sync/shopify.d.ts
+++ extensions/pvx_sync/shopify.d.ts
@@ -23,3 +23,9 @@ declare module './src/InventorySyncAction.jsx' {
   const shopify: import('@shopify/ui-extensions/admin.product-details.action.render').Api;
   const globalThis: { shopify: typeof shopify };
 }
+
+//@ts-ignore
+declare module './src/InventorySyncAction.jsx' {
+  const shopify: import('@shopify/ui-extensions/admin.product-variant-index.selection-action.render').Api;
+  const globalThis: { shopify: typeof shopify };
+}
