fb1fea426eaa4bd7f7e2c5e907e567b256394c8f
Benjamin Cofrancesco <benjamin@whitefoxboutique.com>
2026-06-23 15:31:22 +1000

    feat: types

--- extensions/pvx_sync/shopify.d.ts
+++ extensions/pvx_sync/shopify.d.ts
@@ -17,15 +17,3 @@ declare module './src/ProductStatusBlock.jsx' {
   const shopify: import('@shopify/ui-extensions/admin.product-details.block.render').Api;
   const globalThis: { shopify: typeof shopify };
 }
-
-//@ts-ignore
-declare module './src/InventorySyncAction.jsx' {
-  const shopify: import('@shopify/ui-extensions/admin.product-details.action.render').Api;
-  const globalThis: { shopify: typeof shopify };
-}
-
-//@ts-ignore
-declare module './src/InventorySyncAction.jsx' {
-  const shopify: import('@shopify/ui-extensions/admin.product-index.selection-action.render').Api;
-  const globalThis: { shopify: typeof shopify };
-}
